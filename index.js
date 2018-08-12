69ff0af1afc21379ca335acef9e9270b6c11af57
stikbomb <stikbomb@gmail.com>
2018-08-12 18:08:56 +0300

    Каркас, первый коммит

---
+++ index.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const {ejs} = require('ejs');
+
+const path = require('path');
+
+
+const config = require('./config/index');
+
+const dbURI = "mongodb://" +
+    encodeURIComponent(config.db.username) + ":" +
+    encodeURIComponent(config.db.password) + "@" +
+    config.db.host + ":" +
+    config.db.port + "/" +
+    config.db.name;
+
+mongoose.connect(dbURI, console.log('Succes!'));
+mongoose.Promise = require('bluebird');
+mongoose.set('debug', true);
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+//For public directory
+app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+require('./router/router')(app);
+
+server.listen(8888, () => {
+    console.log('Server started on port 8888');
+});
---
+++ router/router.js
@@ -0,0 +1,8 @@
+"use strict"
+
+module.exports = (app) => {
+
+    app.get('/', (req, res) => {
+        res.send("Welcome to our shop!");
+    })
+};
